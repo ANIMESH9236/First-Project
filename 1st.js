1f55334823b7c47f901fed1f8063d8537463dd4f
ANIMESH9236 <animesh.rai2024@nst.rishihood.edu.in>
2025-01-30 23:06:00 +0530

    firstcommit

---
+++ 1st.js
@@ -0,0 +1,46 @@
+ //js is a loosely type language ie.does not need to specify the data type
+
+ console.log("Hello World! \nhii") ; //the space is also counted 
+ console.log("Hiii");
+ console.log("hello","animesh","rai"); 
+ 
+ //var,let,const are keywords
+ //default value of every variable is undefined
+ 
+ var firstName="Animesh"; //declaration -> assigning value
+ var lastNmame="Rai";
+ console.log(firstName,lastNmame);
+ 
+ let firstname="animesh";
+ let lastname="rai";
+ console.log(firstname,lastname);
+ const firstnaMe="aniMesh"
+ const lastnaMe="rAi"
+ console.log(firstnaMe,lastnaMe)     
+ 
+ var a;
+ var a ;// redeclaration of variable therefore we use let because it does not allow redeclaration, it shows error 
+ 
+ // let a;
+ // let a;  // use let instead of var
+ 
+ var a =10;
+ console.log(a)
+ a=20;
+ console.log(a)
+ 
+ let a =10;
+ console.log(a)
+ a=20;
+ console.log(a)
+ 
+ // const a =10;
+ // console.log(a) // in const both redeclaration and reassigning is not allowed
+ // a=20;
+ // console.log(a)
+ 
+ // var first$="abc";
+ // console.log(first$);  // dollar and underscore are the only special character used in variable declaration;
+ 
+ /*var first.="jkl".     // numbers at the start of variable declaration is not allowed
+  console.log(first.) */
